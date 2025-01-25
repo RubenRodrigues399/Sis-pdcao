e9cb9f7750e17ebc8f9503e47ba91abd8bb0910f
Andrelubambi <andrelubambi36@gmail.com>
2025-01-25 12:59:18 +0100

    pagina de criar agenda

--- src/app/agenda/page.jsx
+++ src/app/agenda/page.jsx
@@ -1,17 +1,86 @@
-import React from 'react'
-import NavBarIn from '@/components/NavBarIn'
-import Footer from '@/components/Footer'
+"use client"
+import React, { useState } from "react";
+import Footer from "@/components/Footer";
+import NavBarIn from "@/components/NavBarIn";
+import LinhaTabelaPaciente from "@/components/LinhaTabelaPacientes";
+import Modal from "@/components/ModalOpen";
 
-function Agenda() {
+const Paciente = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
   return (
     <>
-    <NavBarIn />
-    <div>
-      Agenda do hospital
-    </div>
-    <Footer />
-    </>
-  )
-}
+      <NavBarIn />
+      <div className="flex min-h-screen">
+        <main className="flex-1 bg-gray-100 p-8">
+          <div className="flex items-center justify-center">
+            <section className="bg-white w-11/12 shadow-md p-6 rounded-lg mt-6">
+              <div className="flex justify-between items-center">
+                <h2 className="text-lg font-semibold text-black">Agendas</h2>
+                <button
+                  className="bg-[#21aeb8] w-28 text-white px-4 py-2 rounded"
+                  onClick={() => setModalOpen(true)}
+                >
+                  Adicionar
+                </button>
+              </div>
+              <table className="w-full mt-4 border-collapse">
+                <thead>
+                  <tr className="bg-gray-200 text-gray-900">
+                    <th className="p-2 border">ID</th>
+                    <th className="p-2 border">Médico</th>
+                    <th className="p-2 border">Especialidade</th>
+                    <th className="p-2 border">Data </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <LinhaTabelaPaciente
+                    id={<span className="p-2 text-gray-700">1</span>}
+                    nome={<span className="p-2 text-gray-700">André Lubambi</span>}
+                    genero={<span className="p-2 text-gray-700">Masculino</span>}
+                    data_nascimento={<span className="p-2 text-gray-700">03-03-2000</span>}
+                  />
+                  <LinhaTabelaPaciente
+                    id={<span className="p-2 text-gray-700">2</span>}
+                    nome={<span className="p-2 text-gray-700">Petia Paulo</span>}
+                    genero={<span className="p-2 text-gray-700">Femenino</span>}
+                    data_nascimento={<span className="p-2 text-gray-700">09-08-1999</span>}
+                  />
 
-export default Agenda
+                </tbody>
+              </table>
+            </section>
+          </div>
+        </main>
+      </div>
+
+      <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
+        <span className="text-lg pl-56 font-semibold text-black">Criar agenda</span>
+        <form className="grid grid-cols-2 gap-4 mt-5">
+
+          <select className="border p-2 rounded text-black">
+            <option>Especialidade</option>
+            <option>Ortopedia</option>
+            <option>Estomatologia</option>
+          </select>
+
+          <select className="border p-2 rounded text-black">
+            <option>Médico</option>
+            <option>Ortopedia</option>
+            <option>Estomatologia</option>
+          </select>
+
+
+          <input type="date" placeholder="Date de Nascimento" className="border p-2 rounded text-black" />
+
+        </form>
+        <div className="flex mt-6 justify-center">
+          <button className="bg-[#21aeb8] text-white px-4 py-2 rounded">Criar agenda</button>
+        </div>
+      </Modal>
+      <Footer />
+    </>
+  );
+};
+
+export default Paciente;
