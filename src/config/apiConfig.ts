1e15f260a4922c9d894a9c2ef456e1b984e9c7ca
RubenRodrigues399 <rubenzidane08@gmail.com>
2025-02-17 23:09:52 +0100

    Create apiConfig.ts

---
+++ src/config/apiConfig.ts
@@ -0,0 +1,4 @@
+export const API_CONFIG = {
+  BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://sis-production-4c8f.up.railway.app/sis/",
+  API_KEY: process.env.NEXT_PUBLIC_API_KEY || "",
+};
