aa0b9d8ea2e5df7f9a453fc2d778ef2d22f1be8f
RubenRodrigues399 <rubenzidane08@gmail.com>
2025-02-17 23:10:57 +0100

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { cookies } from 'next/headers';
 
 
-// const API_KEY = "48454de50ba188ebad94f291e743c02af4d853c9" || só usar se tiver problema com "server-onnly!"
+//const API_KEY = "48454de50ba188ebad94f291e743c02af4d853c9" //só usar se tiver problema com "server-onnly!"
 
 export const api = axios.create({
   baseURL: " https://sis-production-4c8f.up.railway.app",
