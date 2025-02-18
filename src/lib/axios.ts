30e31a11015ae71c0a0c45ae3ea517954e064c41
RubenRodrigues399 <rubenzidane08@gmail.com>
2025-02-18 13:35:29 +0100

    Descodificar o token pela api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,24 +2,27 @@ import { API_KEY } from '@/utils/api-key';
 import axios from 'axios';
 import { cookies } from 'next/headers';
 
-
-//const API_KEY = "48454de50ba188ebad94f291e743c02af4d853c9" //só usar se tiver problema com "server-onnly!"
-
 export const api = axios.create({
-  baseURL: " https://sis-production-4c8f.up.railway.app",
+  baseURL: "https://sis-production-4c8f.up.railway.app",
   headers: {
     'Content-Type': 'application/json',
     "APIKEY": `"${API_KEY}"`
   }
 });
 
-
 api.interceptors.request.use(async (config) => {
   const cookieStore = await cookies();
   const authToken = cookieStore.get('sispdcao');
 
   if (authToken?.value) {
-    config.headers.Authorization = `Bearer ${authToken.value}`;
+    // Decodifica o valor do cookie
+    const decodedToken = decodeURIComponent(authToken.value);
+    // Transforma o valor decodificado em JSON
+    const tokenObject = JSON.parse(decodedToken);
+    // Extrai o token do objeto JSON
+    const token = tokenObject.token;
+    
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
