a3f47ca292448a74c898675230b40a2b39c01469
RubenRodrigues399 <rubenzidane08@gmail.com>
2025-02-18 13:36:07 +0100

    Delete apiConfig

--- src/config/apiConfig.ts
+++
@@ -1,4 +0,0 @@
-export const API_CONFIG = {
-  BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://sis-production-4c8f.up.railway.app/sis/",
-  API_KEY: process.env.NEXT_PUBLIC_API_KEY || "",
-};
